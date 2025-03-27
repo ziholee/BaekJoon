b2ae56ed0965698d40cc4a067b1e39fadcf5c0c1
ziholee <leejiho@ijihoui-MacBookPro.local>
2025-03-27 16:49:11 +0900

    feat: 24262 알고리즘 코드 파일 추가

--- 24262/24262.c
+++ 24262/24262.c
@@ -1,5 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(){
-    
+    int n; //횟수를 가리키는 변수.
+    int result = 0;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        result = n/2;
+    }
+    printf("%d\n", n);
+    printf("%d\n", result);
+    return 0;
 }
