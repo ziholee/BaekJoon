284c06272c83c621d247125a4829d934533a2abf
ziholee <brian4574@naver.com>
2025-03-28 23:06:31 +0900

    feat: 10798번 세로읽기

---
+++ 10798/10798.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(){
+    char data [5][15] = {0,}; 
+
+    for(int i = 0; i < 5; i++){
+        scanf("%s", data[i]);
+        if(data[i] == NULL){
+            break;
+        }
+    }
+
+    for(int i = 0; i < 15; i++){
+        for(int j = 0; j < 5; j++){
+            if(data[j][i] != '\0' && data[j][i] != '\n'){
+                printf("%c", data[j][i]);
+            }
+        }
+    }
+    return 0;
+}
