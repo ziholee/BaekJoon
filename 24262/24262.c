9a292f4db3f17d0f783ecbc47be3ed485dcd0e7a
ziholee <brian4574@naver.com>
2025-07-27 23:52:10 +0900

    백준 24262 알고리즘 수업 - 알고리즘의 수행 시간 1 c코드 작성.
    백준 24262 알고리즘 수업 - 알고리즘의 수행 시간 1 c코드 작성.

--- 24262/24262.c
+++ 24262/24262.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
+/*
 int main(){
     int n; //횟수를 가리키는 변수.
     int result = 0;
@@ -12,5 +12,14 @@ int main(){
     }
     printf("%d\n", n);
     printf("%d\n", result);
+    return 0;
+}
+*/
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    printf("1\n");  //코드1 수행 횟수
+    printf("0\n");  //시간 복잡도의 '차수'
     return 0;
 }
