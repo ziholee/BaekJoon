536a9ead40e9d9a52f7b81c88e233a775c03fd89
ziholee <brian4574@naver.com>
2025-10-04 23:01:14 +0900

    백준 5337 웰컴 c코드 작성.

---
+++ 5337/5337.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    /*
+    .  .   .
+    |  | _ | _. _ ._ _  _
+    |/\|(/.|(_.(_)[ | )(/.
+    */
+    printf(".  .   .\n");
+    printf("|  | _ | _. _ ._ _  _\n");
+    printf("|/\\|(/.|(_.(_)[ | )(/.\n");
+
+    return 0;
+}
