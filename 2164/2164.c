177f5d6bebab09ea67a47f6cdd8e724cd4dad36d
ziholee <brian4574@naver.com>
2025-11-22 23:28:51 +0900

    백준 2164 c코드 주석문 추가

--- 2164/2164.c
+++ 2164/2164.c
@@ -5,16 +5,19 @@ int main() {
     int N;
     scanf("%d", &N);
 
-    int queue[1000000];
-    int head = 0;
-    int tail = 0;
+    int queue[1000000];  //큐를 저장할 배열
+    int head = 0;         //큐의 앞쪽 인덱스
+    int tail = 0;         //큐의 뒤쪽 인덱스
 
+    //1부터 N까지의 카드를 큐에 넣는다.
     for(int i = 1; i <= N; i++) {
         queue[tail++] = i;
     }
+    
+    //카드가 한 장 남을 때까지 반복한다.    
     while(tail - head > 1) {
-        head++;
-        queue[tail++] = queue[head++];
+        head++;                          //맨 위 카드를 버린다. 
+        queue[tail++] = queue[head++];   //다음 카드를 맨 뒤로 이동한다.
     }
 
     printf("%d\n", queue[head]);
